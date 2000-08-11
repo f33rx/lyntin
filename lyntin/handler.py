e9f79cb65c5651a5e42a7f6a5eeb01b20431b634
Lyn Headley <193480+laheadle@users.noreply.github.com>
2000-08-11 17:25:29 +0000

    handler class, for output from server.

---
+++ lyntin/handler.py
@@ -0,0 +1,40 @@
+##################################################################
+# This file is part of Lyntin
+# copyright (c) Lyn Headley 1996-1998
+#
+# Lyntin is distributed under the GNU General Public License.  See
+# the file COPYING for details.
+#
+# module handler:
+#
+# a handler intercepts server output at a primitive level,
+# and can respond to it or change the input for the next handler.
+# $Id$
+##################################################################
+
+(CONTINUE, STOP) = range(2)
+
+import mud
+
+class Handler:
+    def handle(self, session, read):
+        """
+        return (continue, read) where continue is either STOP
+        or CONTINUE and read is the input to pass to the next handler
+        """
+        pass
+
+class AppHandler:
+    """
+    the main lyntin app handler
+    """
+    def handle(self, session, read):
+        if not read:
+            read = session.ReadMud()
+
+        if read:
+            # handle actions and displaying stuff from mud
+            mud.HandleMudOutput(read, session)
+
+        return (CONTINUE, read)
+
