b426403965c506bed0304f48f1d1c4cc4420daac
Lyn Headley <193480+laheadle@users.noreply.github.com>
2001-05-25 02:35:29 +0000

    damn

--- lyntin/user.py
+++ lyntin/user.py
@@ -30,26 +30,6 @@ user_custom = {
 # Insert your customizations here
 ##################################################################
 
-import tkgui
-tkgui.fgColorCodes = {
-                "30": "#000000",
-                "31": "#c00000",
-                "32": "#00c000",
-                "33": "#c0c000",
-                "34": "#0000c0",
-                "35": "#c000c0",
-                "36": "#00c0c0",
-                "37": "#c0c0c0",
-                "2030": "#808080",
-                "2031": "#ff6060",
-                "2032": "#00ff00",
-                "2033": "#ffff00",
-                "2034": "#8080ff",
-                "2035": "#ff40ff",
-                "2036": "#00ffff",
-                "2037": "#ffffff" }
-
-tkgui.posixfont = ("Fixedsys", 24)
 
 # uncomment this line to use the examples
 #from examples import *
