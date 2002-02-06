d6d5e96984336da93ecade6e2a668da6a0f35c95
Will Guaraldi <820826+willkg@users.noreply.github.com>
2002-02-06 23:10:24 +0000

    Changes to the README, INSTALL and TODO files. Added a note to the end of the read.cmd file.

--- lyntinng/help/read.cmd
+++ lyntinng/help/read.cmd
@@ -3,3 +3,7 @@
    Will read in a file running all the commands in there.
    Using this and #write allow you to save session settings and
    restore them when you launch Lyntin again.
+
+   You can also read in via the commandline when you start Lyntin:
+
+   lyntin --read 3k
