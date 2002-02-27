f19bbce20c529895e1fd48449a9b8b6602e31380
Will Guaraldi <820826+willkg@users.noreply.github.com>
2002-02-27 03:39:34 +0000

    Some documentation modifications. Moved pythoncode to advanced and overhauled how the #@ stuff works.  It's much cleaner now.  :)

--- lyntinng/modules/advanced.py
+++ lyntinng/modules/advanced.py
@@ -15,6 +15,8 @@ and attempts to execute it in the user.py module.  If no such
 module exists, it executes it in this module.
 """
 
+usermodule = None
+
 def python_cmd(session, words, input):
   """#@ arbitrary python code
 
@@ -22,23 +24,28 @@ def python_cmd(session, words, input):
   some incredibly magic stuff because it requires an environment
   to execute the arbitrary python code in.
   """
-  usermoduleexists = 1
   try:
-    import modules.user
-  except:
-    exported.write_error("No 'user.py' in the modules directory.  Executing locally.")
-    usermoduleexists = 0
-
-  try:
-    if usermoduleexists == 1:
-      exec input[1:] in modules.user.__dict__
-    else:
+    if usermodule == None:
+      exported.write_error("modules.user is either bad or non-existent.  Executing in advanced.py..")
       exec input[1:]
+    else:
+      exec input[1:] in usermodule.__dict__
   except:
     exported.write_error("Error in raw python stuff.")
     exported.write_error(string.join(traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1])))
 
 
+def _import_user_module():
+  """ Imports and returns the modules.user module."""
+  try:
+    import modules.user
+    return modules.user
+  except:
+    return None
+
+
 def load():
   """ Initializes the module by binding all the commands."""
+  import modules.advanced
+  modules.advanced.usermodule = _import_user_module()
   engine.myengine.addCommand("@", python_cmd)
