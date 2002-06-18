e7a2c4198a682f6f9707c5633c39a5eb24ce1dba
Will Guaraldi <820826+willkg@users.noreply.github.com>
2002-06-18 23:41:20 +0000

    Adjusted the test server so it's more dynamic in how we add commands to it.  It also is more informative about the commands it currently understands.

--- lyntinng/errata/testserver.py
+++ lyntinng/errata/testserver.py
@@ -5,7 +5,7 @@
 #
 # Lyntin is distributed under the GNU General Public License license.  See the
 # file LICENSE for distribution details.
-# $Id: testserver.py,v 1.10 2002/05/18 23:32:00 willhelm Exp $
+# $Id: testserver.py,v 1.11 2002/06/01 15:51:44 willhelm Exp $
 #######################################################################
 """
 This runs a multithreaded server on port 3000.
@@ -31,22 +31,14 @@ class ConnectionHandler(SocketServer.StreamRequestHandler):
     self._message = ''
     self._myline = 'Default testserver line.'
 
-    self._commands = {}
-    self._commands["quit"] = self.handle_quit
-    self._commands["command"] = self.handle_command
-    self._commands["remember"] = self.handle_remember
-    self._commands["repeat"] = self.handle_repeat
-    self._commands["help"] = self.handle_command
-    self._commands["colors"] = self.handle_colors
-    self._commands["word"] = self.handle_word
-    self._commands["line"] = self.handle_line
-    self._commands["text"] = self.handle_text
-    self._commands["spam"] = self.handle_spam
-    self._commands["hltest"] = self.handle_coloredline
-
     self._rlist = [self.request]
 
     self._spamThread = None
+
+    self._dir = []
+    for item in dir(self.__class__):
+      if ( type( eval("self.%s" % item)) == type(self.__init__) and item.find("handle_") == 0):
+        self._dir.append(item)
 
   def write(self, data):
     # put data in string for writing when socket ready
@@ -61,6 +53,7 @@ class ConnectionHandler(SocketServer.StreamRequestHandler):
     """
     self.write(self.color("You're logged in.") + "\n")
     import select
+
     try:
       self.request.setblocking(1)
       data = ''
@@ -73,8 +66,8 @@ class ConnectionHandler(SocketServer.StreamRequestHandler):
 
           if data.find("\n") != -1:
             message = data[:data.find("\n")]
-            print "incoming: '%s'" % message
-            self.messageHandler(message)
+            print "incoming: '%s'" % message[:-1]
+            self.messageHandler(message[:-1])
             data = data[data.find("\n") + 1:]
     except Exception, e:
       print "Exception for %s\n%s" % (self.request, e)
@@ -88,31 +81,44 @@ class ConnectionHandler(SocketServer.StreamRequestHandler):
     a command match.
     """
     comm = text.split(" ", 1)[0]
-    if self._commands.has_key(comm):
-      self._commands[comm](text)
+    self.write(self.color(time.ctime() + " <" + str(time.clock()) + ">",32,40) + " '%s'\n" % text)
+    if ("handle_%s" % comm) in self._dir:
+      exec ( "self.handle_%s(text)" % comm)
     else:
       # CATCH ALL for bad commands
-      self.write(self.color("huh?", 33) + "\n")
-    self.write(self.color(time.ctime() + " <" + str(time.clock()) + ">",32,40) + "\n")
+      self.write(self.color("received unimplemented command '%s'" % comm, 33) + "\n")
 
   def handle_quit(self, text):
-    # QUIT command -> ends session 
+    """ Quits your session."""
     self.write("bye bye\n")
     raise ValueError, "Shutdown of connection requested."
     
   def handle_remember(self, text):
-    # REMEMBER command
+    """ Forces us to memorize something to "repeat" back to you later."""
     self._myline = text[text.find(" ")+1:]
 
   def handle_repeat(self, text):
+    """ Has us repeat something we "remember"."""
     self.write(self._myline + "\n")
 
   def handle_command(self, text):
-    # COMMAND command -> see startup for adding new commands
-    self.write(string.join(self._commands, "\n") + "\n")
+    """ Prints out all the commands we understand."""
+    commands = []
+    for mem in self._dir:
+      if mem.find("handle_") == 0:
+        doc = ""
+        try: doc = eval ("self.%s.__doc__" % mem)
+        except: pass
+
+        if doc:
+          commands.append(mem[7:] + " - " + doc)
+        else:
+          commands.append(mem[7:])
+
+    self.write(string.join(commands, "\n") + "\n")
 
   def handle_colors(self, text):
-    # COLORS command -> puts out 8 lines with all the colors displayed
+    """ Prints out all the colors we know about."""
     response = ''
     for background in range(40,48):
       for foreground in range(30,38):
@@ -123,18 +129,31 @@ class ConnectionHandler(SocketServer.StreamRequestHandler):
     self.write(response)
 
   def handle_word(self, text):
-    # WORD command -> returns one random word
+    """ Returns a random word."""
     response = ''
     response = self.getWords(1) + "\n"
     self.write(response)
 
   def handle_line(self, text):
-    # LINE command -> returns 10 random words
+    """ Returns a line consisting of 10 random words."""
     response = ''
     response = self.getWords(10) + "\n"
     self.write(response)
       
+  def handle_paragraph(self, text):
+    """ Returns a paragraph, which coincidentally, is a description of Lyntin."""
+    output = ("Lyntin is a mud client that is written in Python and uses\n" +
+             "Python as a scripting language. It strives to be functionally\n" +
+             "similar to TinTin++ while enhancing that functionality with\n" +
+             "the ability to call Python functions directly from the input\n" +
+             "line. It has the advantage of being platform-independent and\n" +
+             "has multiple interfaces as well--I use Lyntin at home with\n" +
+             "the Tk interface as well as over telnet using the text\n" +
+             "interface.")
+    self.write(output)
+
   def handle_coloredline(self, text):
+    """ Returns a series of lines with colored text."""
     output = "Notadragon is not a dragon.\n"
     output += "Notadragon is " + self.color("not", 32) + " a dragon.\n"
     output += "N" + self.color("otadragon", 35) + " is not a " + self.color("dragon.", 33) + "\n"
@@ -142,14 +161,14 @@ class ConnectionHandler(SocketServer.StreamRequestHandler):
     self.write(output)
 
   def handle_text(self, text):
-    # TEXT command -> returns 10 lines of 10 words
+    """ Returns 10 lines each with 10 random words in it."""
     response = ''
     for x in range(0,9):
       response += self.getWords(10) + "\n"
     self.write(response)
 
   def handle_spam(self, text):
-    # SPAM command -> starts timed spam
+    """ Starts the spam command."""
     # 1 = 1 line per sec, 2 = 5 lines per sec, 3 = 10 lines per sec
     text = text.split()
     
