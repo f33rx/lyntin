10b231b3b67b787c5914e72207eecd8efafbc5e3
Joshua Berne <30129762+notadragon@users.noreply.github.com>
2002-10-26 03:33:02 +0000

    uncommented a slightly essential line for importing modules that are in subpackages, since __import__ doesn't return what one would assume it returns.

--- lyntinng/modules/advanced.py
+++ lyntinng/modules/advanced.py
@@ -4,7 +4,7 @@
 #
 # Lyntin is distributed under the GNU General Public License license.  See the
 # file LICENSE for distribution details.
-# $Id: advanced.py,v 1.20 2002/10/23 23:59:08 willhelm Exp $
+# $Id: advanced.py,v 1.21 2002/10/25 23:44:29 willhelm Exp $
 #######################################################################
 """
 This module holds the magical python_cmd code.  It takes in code,
@@ -111,7 +111,8 @@ def import_cmd(session, args, input):
     try:
       _module = __import__( mod )
 
-      # _module = sys.modules[mod]
+      _module = sys.modules[mod]
+
       if (_module.__dict__.has_key("load")):
         _module.load()
 
