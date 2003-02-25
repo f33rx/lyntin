d3cddd6fa2fed504fcbae7dbcd487bfa6f2b002d
Will Guaraldi <820826+willkg@users.noreply.github.com>
2003-02-25 00:15:44 +0000

    This is the testmodule, tests, and all the benchmarks.
    To use:
    
       #load modules.testsystem.testmodule
    
    and then:
    
       #runtest
    
    If there is no benchmark for a given test (which is just a command
    file), then it'll create one.  If there is a benchmark, it'll diff the
    results it just got with those in the benchmark and let you know whether
    the test passed or failed.

---
+++ lyntinng/modules/testsystem/__init__.py
@@ -0,0 +1,11 @@
+#######################################################################
+# This file is part of Lyntin
+# copyright (c) Free Software Foundation 2001, 2002
+#
+# Lyntin is distributed under the GNU General Public License license.  See the
+# file LICENSE for distribution details.
+# $Id$
+#######################################################################
+"""
+This directory contains the testmodule, tests, and benchmarks.
+"""
