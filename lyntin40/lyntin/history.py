26abb491ca1687e0f646efc7c557489a67aef1c3
Will Guaraldi <820826+willkg@users.noreply.github.com>
2003-06-08 16:14:30 +0000

    Added named history functinoality. Added another global variable.

--- lyntin40/lyntin/history.py
+++ lyntin40/lyntin/history.py
@@ -4,7 +4,7 @@
 #
 # Lyntin is distributed under the GNU General Public License license.  See the
 # file LICENSE for distribution details.
-# $Id: history.py,v 1.1.1.1 2003/04/29 21:42:35 willhelm Exp $
+# $Id: history.py,v 1.1 2003/05/05 05:54:19 willhelm Exp $
 #######################################################################
 """
 The HistoryManager keeps track of the last 1000 lines of user input 
@@ -51,6 +51,9 @@ class HistoryManager(manager.Manager):
       try:
         returninput = self._history[int(index)]
       except:
+        for h in self._history:
+          if h[0:len(index)] == index:
+            return h
         return None
 
     # check to see if they want to do a substitution
