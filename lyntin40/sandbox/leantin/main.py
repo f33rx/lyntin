6429d0a9314d99dff2c954d6b31eca1eb964975c
odbrazen <odbrazen@users.sourceforge.net>
2004-05-04 05:02:04 +0000

    * rtelnetlib.py added, better replacement for telnetlib * sock, session updated to use rtelnetlib * support for NAWS, ECHO, EOR, TTYPE * tweaks to tkui * tweaks to ansi handling

--- lyntin40/sandbox/leantin/main.py
+++ lyntin40/sandbox/leantin/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python2.3
 # python imports
-import socket, Queue, time
 # our imports
-import sock, ui, tkui, mudcommands, engine, catcher, libmisc
+import engine
 import session
-import mytk
+import tkui
 
-__version__ = "$Revision: 1.9 $"
+__version__ = "$Revision: 1.10 $"
 
 """This is the main loop for leanlyn, currently it is just a test harness"""
 
